1a50dd9a77d36b7c243e6b7d8001a61116e882b4
Eamonn Smyth <e.smyth75@gmail.com>
2020-11-10 10:38:30 +0000

    Add a todo list and an add todo page

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
-
 def get_todo_list(request):
     """get_todo_list:
+
+    Return the todo list to the template
 
     \n Args:
     1.  request: A user entered http request in the address bar
@@ -12,4 +14,23 @@ def get_todo_list(request):
     \n Returns:
     1. the todo_list.html template.
     """
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    """add_item:
+
+    Return the add_item template
+
+    \n Args:
+    1.  request: A user entered http request in the address bar
+
+    \n Returns:
+    1. the add_item.html template.
+    """
+
+    return render(request, 'todo/add_item.html')
